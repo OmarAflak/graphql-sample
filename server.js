528c6d51438ea64ff0de015bb0c02feee2df7e0f
Omar Aflak <aflakomar@gmail.com>
2018-04-25 15:45:59 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+    type Query {
+        hello(name: String!): String
+        random(count: Int): [Float!]
+    }
+`);
+
+var root = {
+    hello: ({name}) => {
+        return 'Hello '+name+'!';
+    },
+    random: ({count}) => {
+        var array = [];
+        for(var i=0 ; i<(count || 1) ; i++){
+            array.push(Math.random());
+        }
+        return array;
+    }
+}
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
